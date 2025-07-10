50f70bb7712257cab299417e50ebe9e92c25e8fc
Shim2003 <shimkh-wm23@student.tarc.edu.my>
2025-07-10 13:16:53 +0800

    Initial

---
+++ src/tarcare/medical/centre/TARCareMedicalCentre.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarcare.medical.centre;
+
+/**
+ *
+ * @author jecsh
+ */
+public class TARCareMedicalCentre {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
