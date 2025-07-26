9130797ec5e709d49bfa4d2519d7abf0035808fd
Hhehhe37 <142575377+Hhehhe37@users.noreply.github.com>
2025-07-26 12:29:28 +0800

    UPDATE

---
+++ src/Utility/UtilityClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Utility;
+
+/**
+ *
+ * @author user
+ */
+public class UtilityClass {
+
+    public static String truncate(String str, int maxLength) {
+        if (str.length() <= maxLength) {
+            return str;
+        } else {
+            return str.substring(0, maxLength - 3) + "...";
+        }
+    }
+
+}
